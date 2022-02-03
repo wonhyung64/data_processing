8bdee848c98bf717ab02e5b2d8e7e2cafa803537
WonHyung Shin <wonhyung64@icloud.com>
2022-02-03 12:57:04 +0900

    Update ship.py

--- ship.py
+++ ship.py
@@ -3,6 +3,7 @@ import os
 import json
 import numpy as np
 import xml.etree.ElementTree as elemTree
+import re
 import tensorflow as tf
 from PIL import Image
 
@@ -13,6 +14,7 @@ def serialize_example(dic):
     bbox = dic["bbox"].tobytes()
     bbox_shape = np.array(dic["bbox_shape"]).tobytes()
     label = dic["label"].tobytes()
+    filename = dic["filename"].tobytes()
 
     dic = tf.train.Example(features=tf.train.Features(feature={
         'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image])),
@@ -20,6 +22,7 @@ def serialize_example(dic):
         'bbox': tf.train.Feature(bytes_list=tf.train.BytesList(value=[bbox])),
         'bbox_shape': tf.train.Feature(bytes_list=tf.train.BytesList(value=[bbox_shape])),
         'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[label])),
+        'filename': tf.train.Feature(bytes_list=tf.train.BytesList(value=[filename])),
     })) 
     return dic.SerializeToString()
 
@@ -31,6 +34,7 @@ def deserialize_example(serialized_string):
         'bbox': tf.io.FixedLenFeature([], tf.string), 
         'bbox_shape': tf.io.FixedLenFeature([], tf.string), 
         'label': tf.io.FixedLenFeature([], tf.string), 
+        'filname': tf.io.FixedLenFeauture([], tf.string),
     } 
     example = tf.io.parse_single_example(serialized_string, image_feature_description) 
 
@@ -39,118 +43,142 @@ def deserialize_example(serialized_string):
     bbox = tf.io.decode_raw(example["bbox"], tf.float32)
     bbox_shape = tf.io.decode_raw(example["bbox_shape"], tf.int32)
     label = tf.io.decode_raw(example["label"], tf.int32) 
+    filename = tf.io.decode_raw(example["filename"], tf.int32)
 
     image = tf.reshape(image, image_shape)
     bbox = tf.reshape(bbox, bbox_shape)
     
-    return image, bbox, label
+    return image, bbox, label, filename
 #%%
-# filename_lst = []
+def write_labels(save_dir, labels):
+    with open(f"{save_dir}/labels.txt", "w") as f:
+        for label in labels:
+            f.write(label+",")
 
-# save_dir = r"D:\won\ship_tfrecord"
-# name = 'train'
-# writer = tf.io.TFRecordWriter(f'{save_dir}\{name}.tfrecord'.encode("utf-8"))
+#%%
+def save_dict_to_file(dic,dict_dir):
+    f = open(dict_dir + '/label_dict.txt', 'w')
+    f.write(str(dic))
+    f.close()
+#%%
+def read_labels(label_dir):
+    f = open(f"{label_dir}/labels.txt", "r")
+    labels = f.read().split(",")
+    del labels[-1]
+    return labels
+#%%
+save_dir = "/Users/wonhyung64/data/ship"
+file_dir = "/Users/wonhyung64/data"
+# file_dir = "C:/won/data"
+split = "train"
 
-# work_dir = r"C:\won\data\ship_detection\train\남해_여수항1구역_BOX"
-# work_contents = os.listdir(work_dir)
-# for i in range(len(work_contents)):
-#     path_dir = work_dir + r"\\" + work_contents[i]
-#     path_contents = os.listdir(path_dir)
-#     for j in range(len(path_contents)):
-#         data_dir = path_dir + r"\\" + path_contents[j]
-#         data_contents = os.listdir(data_dir)
-#         filename_lst = list(set([data_contents[l][:25] for l in range(len(data_contents))]))
+if os.path.exists(f"{save_dir}/{split}.tfrecord".encode("utf-8")) == False:
+    try_num = 0
+    label_dict = {}
+    filename_lst = []
+    file_dir1 = file_dir
 
-#         for k in range(len(filename_lst)):
-#             file_dir = data_dir + r"\\" + filename_lst[k]
-#             filename = filename_lst[k]
-#             # filename_lst.append(filename)
+    writer1 = tf.io.TFRecordWriter(f'{save_dir}/train.tfrecord'.encode("utf-8"))
+    writer2 = tf.io.TFRecordWriter(f'{save_dir}/test.tfrecord'.encode("utf-8"))
 
-#             #jpg
-#             img_ = Image.open(file_dir + ".jpg")
-#             img_ = tf.convert_to_tensor(np.array(img_, dtype=np.int32)) / 255 # image
-#             img_ = tf.image.resize(img_, (432, 768))
-#             img = np.array(img_)
+    file_dir2 = file_dir1 + "/ship_detection/train/남해_여수항1구역_BOX"
+    file_dir2_conts = os.listdir(file_dir2)
+    file_dir2_conts.remove(".DS_Store")
 
-#             #xml
-#             tree = elemTree.parse(file_dir + ".xml")
-#             root = tree.getroot()
-#             bboxes_ = []
-#             labels_ = []
-#             for x in root:
-#                 # print(x.tag)
-#                 if x.tag == "object":
-#                     for y in x:
-#                         # print("--", y.tag)
-#                         if y.tag == "bndbox":
-#                             bbox_ = [int(z.text) for z in y] 
-#                             bbox = [bbox_[0] / 2160, bbox_[1] / 3840, bbox_[2] / 2160, bbox_[3] / 3840]
-#                             # print("----", bbox)
-#                             bboxes_.append(bbox)
-#                         if y.tag == "category_id":
-#                             # print("----", y.text)
-#                             labels_.append(int(y.text))
-#             bboxes = np.array(bboxes_, dtype=np.float32)
-#             labels = np.array(labels_, dtype=np.int32)
+    for i in range(len(file_dir2_conts)):
+        file_dir3 = file_dir2 + "/" + file_dir2_conts[i]
+        file_dir3_conts = os.listdir(file_dir3)
+        if ".DS_Store" in file_dir3_conts: file_dir3_conts.remove(".DS_Store")
 
-#             #json
-#             with open(file_dir + "_meta.json", "r", encoding="UTF8") as st_json:
-#                 st_python = json.load(st_json)
-#             st_python["Date"]
-#             time = st_python["Date"][11:-1]
-#             weather = st_python["Weather"]
-#             season = st_python["Season"]
+        for j in range(len(file_dir3_conts)):
+            file_dir4 = file_dir3 + "/" + file_dir3_conts[j]
+            file_dir4_conts = os.listdir(file_dir4)
+            if ".DS_Store" in file_dir4_conts: file_dir4_conts.remove(".DS_Store")
+            filename_lst = list(set([file_dir4_conts[l][:33] for l in range(len(file_dir4_conts))]))
 
-#             #to_dictionary
-#             dic = {
-#                 "image":img,
-#                 "image_shape":img.shape,
-#                 "bbox":bboxes,
-#                 "bbox_shape":bboxes.shape,
-#                 "label":labels,
-#             }
+            for k in range(len(filename_lst)):
+                try_num += 1
+                if try_num % 3 == 0:
+                    print(try_num)
+                    file_dir5 = file_dir4 + "/" + filename_lst[k]
+                    filename = re.sub(r'[^0-9]', '', filename_lst[k])
+                    filename_lst.append(filename)
 
-#             # info_ = {
-#             #     "filename":filename,
-#             #     "time":time,
-#             #     "weather":weather,
-#             #     "season":season,
-#             #     "time":time,
-#             #     "weather":weather,
-#             #     "season":season
-#             # }
-#             # info = np.array([info_])
+                    #jpg
+                    img_ = Image.open(file_dir5 + ".jpg")
+                    img_ = tf.convert_to_tensor(np.array(img_, dtype=np.int32)) / 255 # image
+                    img_ = tf.image.resize(img_, (432, 768))
+                    img = np.array(img_)
 
-#             writer.write(serialize_example(dic))
+                    #xml
+                    tree = elemTree.parse(file_dir5 + ".xml")
+                    root = tree.getroot()
+                    bboxes_ = []
+                    labels_ = []
+                    for x in root:
+                        # print(x.tag)
+                        if x.tag == "object":
+                            for y in x:
+                                # print("--", y.tag)
+                                if y.tag == "bndbox":
+                                    bbox_ = [int(z.text) for z in y] 
+                                    bbox = [bbox_[0] / 2160, bbox_[1] / 3840, bbox_[2] / 2160, bbox_[3] / 3840]
+                                    # print("----", bbox)
+                                    bboxes_.append(bbox)
+                                if y.tag == "category_id":
+                                    # print("----", y.text)
+                                    label = int(y.text)
+                                    labels_.append(label)
+                                if y.tag == "name": 
+                                    label_dict[str(label)] = y.text
+                    bboxes = np.array(bboxes_, dtype=np.float32)
+                    labels = np.array(labels_, dtype=np.int32)
 
-#             # info_dir = r'C:\won\data\ship_detection\datasets\info\\' + filename
-#             # np.save(info_dir + ".npy", info, allow_pickle=True)
+                    #json
+                    with open(file_dir5 + "_meta.json", "r", encoding="UTF8") as st_json:
+                        st_python = json.load(st_json)
+                    st_python["Date"]
+                    time = st_python["Date"][11:-1]
+                    weather = st_python["Weather"]
+                    season = st_python["Season"]
 
-# # filename_lst = np.array(filename_lst)
-# # filename_dir = r'C:\won\data\ship_detection\datasets\filenames.npy'
-# # np.save(filename_dir, filename_lst, allow_pickle=True)
+                    #to_dictionary
+                    dic = {
+                        "image":img,
+                        "image_shape":img.shape,
+                        "bbox":bboxes,
+                        "bbox_shape":bboxes.shape,
+                        "label":labels,
+                        "filename":np.array(filename)
+                    }
+
+                    info_ = {
+                        "time":time,
+                        "weather":weather,
+                        "season":season,
+                    }
+
+                    info = np.array([info_])
+                    if try_num % 50 == 0 : writer2.write(serialize_example(dic))
+                    else:writer1.write(serialize_example(dic))
+
+                    if os.path.isdir(save_dir + "/meta") == False: os.mkdir(save_dir + "/meta")
+
+                    info_dir = save_dir + "/meta/" + filename
+                    np.save(info_dir + ".npy", info, allow_pickle=True)
+
+    save_dict_to_file(label_dict, save_dir)
+
+# datasets = tf.data.TFRecordDataset(f"{save_dir}/{split}.tfrecord".encode("utf-8")).map(deserialize_example)
+
+'''
+라벨 딕셔너리 키 순으로 정렬후 value만 추출
+라벨 txt로 저장
+deserialized 되는지 확인
+def 로  fetch 함수 만들기
+연구실로 이동 후 학습 
+
+'''
+
 
 # %%
-#%%visualization
-# import matplotlib.pyplot as plt
-# from PIL import ImageDraw
-# image = tf.keras.preprocessing.image.array_to_img(img)
-# draw = ImageDraw.Draw(image)
-# for bbox in bboxes:
-#      x1, y1, x2, y2 = tf.split(bbox, 4, axis=-1)
-#      x1, y1, x2, y2 = x1*432, y1*768, x2*432, y2*768
-#      draw.rectangle((x1, y1, x2, y2), outline=(0, 255, 0), width=2)
-# plt.figure()
-# plt.imshow(image)
-# plt.show()
-
-# data_dir = r"C:\won\data\ship_detection"
-# name = 'train'
-# sample = tf.data.TFRecordDataset(f"{data_dir}/{name}.tfrecord".encode("utf-8")).map(deserialize_example)
-# sample = sample.shuffle(buffer_size=30000, reshuffle_each_iteration=True)
-# padding_values = (tf.constant(0, tf.float32), tf.constant(0, tf.float32), tf.constant(-1, tf.int32))
-# data_shapes = ([None, None, None], [None, None], [None,])
-# dataset = sample.padded_batch(4, data_shapes, padding_values, drop_remainder=True)
-# for i in dataset.take(1):
-#     break
-# i
